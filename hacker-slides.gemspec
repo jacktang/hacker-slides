121c23bf56044c49ad0f73c02956d6d315accce5
jack.tang <himars@gmail.com>
2009-04-08 03:04:32 +0800

    install bundles on line

--- hacker-slides.gemspec
+++ hacker-slides.gemspec
@@ -1,4 +1,16 @@
-require File.dirname(__FILE__) + '/lib/hacker_slides/version'
+#require File.dirname(__FILE__) + '/lib/hacker_slides/version'
+
+module HackerSlides
+  class Version
+    MAIN  = 1; MAJOR = 0; MINOR = 1
+    class << self
+      def value
+        return "#{MAIN}.#{MAJOR}.#{MINOR}"
+      end
+    end
+  end
+end
+
 
 spec = Gem::Specification.new do |s|
   s.name = 'hacker-slides'
@@ -18,5 +30,30 @@ spec = Gem::Specification.new do |s|
   s.executables = ['hacker-slides']
 
   # ruby -rpp -e' pp `git ls-files`.split("\n") '
-  s.files = Dir['bundles/*.bundle'] + Dir['samples/sample.*'] + Dir['lib/**/*.rb'] + Dir['spec/**/*.rb'] + ["LICENSE", "README.textile"]
+  #s.files = Dir['bundles/*.bundle'] + Dir['samples/sample.*'] + Dir['lib/**/*.rb'] + Dir['spec/**/*.rb'] + ["LICENSE", "README.textile"]
+  s.files = [ 
+              'samples/sample.textile',
+              'samples/sample.markdown',
+              'bundles/s5-simple.bundle',
+              'lib/hacker_slides.rb',
+              'lib/hacker_slides/bundle.rb',
+              'lib/hacker_slides/presentation.rb',
+              'lib/hacker_slides/version.rb',
+              'lib/hacker_slides/engine.rb',
+              'lib/hacker_slides/engine/base.rb',
+              'lib/hacker_slides/engine/markup_engine.rb',
+              'lib/hacker_slides/engine/render_engine.rb',
+              'lib/hacker_slides/engine/s5_slides_generator.rb',
+              'lib/hacker_slides/engine/slides_engine.rb',
+              'lib/hacker_slides/helper.rb',
+              'lib/hacker_slides/helper/content_helper.rb', 
+              'lib/minitar.rb',
+              'lib/minitar/posix_header.rb',
+              'lib/minitar/input.rb',
+              'lib/minitar/output.rb',
+              'lib/minitar/reader.rb',
+              'lib/minitar/writer.rb',
+              'LICENSE',
+              'README.textile'
+            ]
 end
--- lib/hacker_slides/bundle.rb
+++ lib/hacker_slides/bundle.rb
@@ -1,14 +1,42 @@
 require 'rubygems'
 require 'zlib'
+require 'open-uri'
+require 'fileutils'
+
 require 'minitar'
 
 module HackerSlides
   class Bundle
     class << self
 
+      # install remote/local bundle 
+      def install(src, bundle)
+        
+        bundle = "#{bundle}.bundle" if(File.extname(bundle) != '.bundle')
+        
+        if(src)
+          bundle_path = File.join(src, bundle)
+        else
+          bundle_path = File.expand_path(bundle)
+        end
+        dest = File.join(usr_bundle_dir, bundle)
+        open(bundle_path) do |stream|
+          File.open(dest, 'wb') do |f|
+            f.write(stream.read)
+          end
+        end
+
+      end
+
       def list_bundles
         builtin_bundles = []
         usr_bundles = []
+
+        Dir.glob(File.join(usr_bundle_dir, '*')).each do |bundle|
+          bundle_name = File.basename(bundle, '.*')
+          usr_bundles << bundle_name 
+        end
+
         Dir.glob(File.join(builtin_bundle_dir, '*')).each do |bundle|
           bundle_name = File.basename(bundle, '.*')
           builtin_bundles << bundle_name 
@@ -20,10 +48,37 @@ module HackerSlides
         return File.expand_path(File.dirname(__FILE__) + '/../../bundles')
       end
 
-      def lookup_bundle_path(bundle_name)
-        return File.join(builtin_bundle_dir, "#{bundle_name}.bundle")
+      def usr_bundle_dir
+        bundle_dir = File.join(cache_dir, 'bundles')
+        unless File.exists?(bundle_dir)
+          FileUtils.mkdir(bundle_dir)
+        end
+        return bundle_dir
       end
 
+      def lookup_bundle_path(bundle_name)
+        bundle = File.join(usr_bundle_dir, "#{bundle_name}.bundle")
+        if(File.exists?(bundle))
+          return bundle
+        else
+          bundle = File.join(builtin_bundle_dir, "#{bundle_name}.bundle") 
+          return bundle if(File.exists?(bundle))
+        end
+      end
+
+      def cache_dir
+        if(PLATFORM =~ /win32/)
+          dir = File.join(ENV['USERPROFILE'], '.hackerslides')
+        else
+          dir = File.join(File.expand_path("~"), ".hackerslides")
+        end
+
+        unless File.exist?(dir)
+          FileUtils.mkdir(dir)
+        end
+        return dir
+      end
+      
       def extract_files_from_bundle(bundle, files, dest)
         Zlib::GzipReader.open(bundle) do |tgz|
           Archive::Tar::Minitar.unpack(tgz, dest, files)
@@ -53,7 +108,6 @@ module HackerSlides
         end
         return nil
       end
-
 
     end
   end
